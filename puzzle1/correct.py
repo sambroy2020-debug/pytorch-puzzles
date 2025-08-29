5254928fa9478048273434fb0f3f7ecb6bfe2df2
Sam Roy <sambroy2020@gmail.com>
2025-08-29 01:26:53 +0000

    finishing puzzle 1

---
+++ puzzle1/correct.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class DynamicNet(nn.Module):
+    def __init__(self, input_size, output_size, hidden_sizes):
+        super().__init__()
+        # Corrected: Using nn.ModuleList
+        self.layers = nn.ModuleList()
+        
+        layer_sizes = [input_size] + hidden_sizes
+        for i in range(len(layer_sizes) - 1):
+            self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+            self.layers.append(nn.ReLU())
+            
+        self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return self.output_layer(x)
+
+# Setup
+model = DynamicNet(input_size=20, output_size=2, hidden_sizes=[32, 64])
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+input_tensor = torch.randn(10, 20)
+target = torch.empty(10, dtype=torch.long).random_(2)
+loss_fn = nn.CrossEntropyLoss()
+
+initial_hidden_weights = model.layers[0].weight.clone()
+
+# Training step
+optimizer.zero_grad()
+output = model(input_tensor)
+loss = loss_fn(output, target)
+loss.backward()
+optimizer.step()
+
+print("Hidden layer weights updated:", not torch.equal(initial_hidden_weights, model.layers[0].weight))
+print("Number of parameters found by optimizer:", len(list(model.parameters())))
